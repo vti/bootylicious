5ecfe38e0799146a52d6a9452d3e411ba976e4ec
vti <viacheslav.t@gmail.com>
2010-10-28 14:57:52 +0200

    Cleanup

--- lib/Bootylicious/Plugin/HttpCache.pm
+++ lib/Bootylicious/Plugin/HttpCache.pm
@@ -10,27 +10,25 @@ use Mojo::ByteStream;
 sub register {
     my ($self, $app) = @_;
 
-    $app->plugins->add_hook(after_dispatch => \&_set_etag_header);
+    $app->plugins->add_hook(
+        after_dispatch => sub {
+            my ($self, $c) = @_;
+
+            return unless $c->req->method eq 'GET';
+
+            my $body = $c->res->body;
+            return unless defined $body;
+
+            my $our_etag = Mojo::ByteStream->new($body)->md5_sum;
+            $c->res->headers->header('ETag' => $our_etag);
+
+            my $browser_etag = $c->req->headers->header('If-None-Match');
+            return unless $browser_etag && $browser_etag eq $our_etag;
+
+            $c->res->code(304);
+            $c->res->body('');
+        }
+    );
 }
-
-sub _set_etag_header {
-    my ($self, $c) = @_;
-
-    return unless $c->req->method eq 'GET';
-
-    my $body = $c->res->body;
-    return unless defined $body;
-
-    my $our_etag = _calculate_etag($body);
-    $c->res->headers->header('ETag' => $our_etag);
-
-    my $browser_etag = $c->req->headers->header('If-None-Match');
-    return unless $browser_etag && $browser_etag eq $our_etag;
-
-    $c->res->code(304);
-    $c->res->body('');
-}
-
-sub _calculate_etag { Mojo::ByteStream->new(shift)->md5_sum }
 
 1;
